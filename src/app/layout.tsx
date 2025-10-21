1477189ec7fc7e27483db8c3a4f73da492505d00
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2025-10-21 14:13:47 +0200

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,11 @@ export default async function RootLayout({
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/qbn0cgq.css"></link>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className="overflow-x-hidden antialiased">
         <Header />
